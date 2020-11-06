f09c06d507c6e0b3cd09a7c715b7d4c731411050
wo80 <christian.woltering@tu-dortmund.de>
2020-11-06 02:00:30 +0100

    Add CompressedColumnStorage extensions

---
+++ CSparse.Extensions.Tests/Storage/CompressedColumnStorageTests.cs
@@ -0,0 +1,53 @@
+﻿
+namespace CSparse.Tests.Storage
+{
+    using CSparse.Double;
+    using NUnit.Framework;
+    using System.Linq;
+
+    public class CompressedColumnStorageTests
+    {
+        [Test]
+        public void TestCountPredicate()
+        {
+            var A = SparseMatrix.OfRowMajor(3, 3, new double[]
+            {
+                1.0, 0.0, 1.0,
+                0.0, 2.0, 0.5,
+                1.0, 0.5, 3.0
+            });
+
+            // Only 2 entries should be returned (zeros shouldn't be in the CSC storage).
+            Assert.AreEqual(2, A.Count((i, j, a) => a < 1.0));
+
+            var D = SparseMatrix.OfRowMajor(3, 3, new double[]
+            {
+                1.0, 0.0, 0.0,
+                0.0, 2.0, 0.0,
+                0.0, 0.0, 3.0
+            });
+
+            // Check for non-diagonal entries.
+            Assert.AreEqual(0, D.Count((i, j, a) => i != j));
+        }
+
+        [Test]
+        public void TestEnumerateIndexedPredicate()
+        {
+            var A = SparseMatrix.OfRowMajor(3, 2, new double[]
+            {
+                1.0, 0.0,
+                0.0, 2.0,
+                1.0, 0.5
+            });
+
+            var actual = A.EnumerateIndexed((i, j, a) => a < 1.0).ToArray();
+
+            Assert.AreEqual(1, actual.Length);
+
+            Assert.AreEqual(2, actual[0].Item1);
+            Assert.AreEqual(1, actual[0].Item2);
+            Assert.AreEqual(0.5, actual[0].Item3);
+        }
+    }
+}
---
+++ CSparse.Extensions/Storage/CompressedColumnStorageExtensions.cs
@@ -0,0 +1,74 @@
+﻿namespace CSparse
+{
+    using CSparse.Storage;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// CompressedColumnStorage extension methods.
+    /// </summary>
+    public static class CompressedColumnStorageExtensions
+    {
+        /// <summary>
+        /// Count all matrix entries that match the given predicate.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="matrix">The matrix.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns></returns>
+        public static int Count<T>(this CompressedColumnStorage<T> matrix, Func<int, int, T, bool> predicate)
+            where T : struct, IEquatable<T>, IFormattable
+        {
+            int columns = matrix.ColumnCount;
+
+            var ax = matrix.Values;
+            var ap = matrix.ColumnPointers;
+            var ai = matrix.RowIndices;
+
+            int count = 0;
+
+            for (int i = 0; i < columns; i++)
+            {
+                int end = ap[i + 1];
+                for (int j = ap[i]; j < end; j++)
+                {
+                    if (predicate(ai[j], i, ax[j]))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Enumerate all matrix entries that match the given predicate.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="matrix">The matrix.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns></returns>
+        public static IEnumerable<Tuple<int, int, T>> EnumerateIndexed<T>(this CompressedColumnStorage<T> matrix, Func<int, int, T, bool> predicate)
+            where T : struct, IEquatable<T>, IFormattable
+        {
+            int columns = matrix.ColumnCount;
+
+            var ax = matrix.Values;
+            var ap = matrix.ColumnPointers;
+            var ai = matrix.RowIndices;
+
+            for (int i = 0; i < columns; i++)
+            {
+                int end = ap[i + 1];
+                for (int j = ap[i]; j < end; j++)
+                {
+                    if (predicate(ai[j], i, ax[j]))
+                    {
+                        yield return new Tuple<int, int, T>(ai[j], i, ax[j]);
+                    }
+                }
+            }
+        }
+    }
+}
