185dd0d351cc4a4970bf551e3c0b184699e77911
wo80 <christian.woltering@tu-dortmund.de>
2022-03-08 15:11:46 +0100

    Add MINRES solver.

---
+++ CSparse.Extensions.Tests/Double/Solvers/MinResTest.cs
@@ -0,0 +1,39 @@
+﻿
+namespace CSparse.Extensions.Tests.Double.Solvers
+{
+    using CSparse.Double;
+    using CSparse.Double.Preconditioner;
+    using CSparse.Double.Solvers;
+    using CSparse.Solvers;
+    using NUnit.Framework;
+    using System.Collections.Generic;
+
+    public class MinResTest
+    {
+        [Test]
+        public void TestSolve()
+        {
+            const int N = 100;
+
+            var A = CreateSparse.RandomSymmetric(N, 0.1, true);
+            var x = Vector.Create(N, 1.0);
+            var b = new double[N];
+
+            A.Multiply(x, b);
+
+            var iterator = new Iterator<double>(new List<IIterationStopCriterion<double>>()
+            {
+                new IterationCountStopCriterion<double>(N),
+                new ResidualStopCriterion<double>(1e-8)
+            });
+
+            Vector.Clear(x);
+
+            var solver = new MinRes();
+
+            solver.Solve(A, b, x, iterator, new DiagonalPreconditioner(A));
+
+            Assert.AreEqual(iterator.Status, IterationStatus.Converged);
+        }
+    }
+}
---
+++ CSparse.Extensions/Solvers/IteratorExtensions.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CSparse.Solvers
+{
+    using System;
+
+    static class IteratorExtensions
+    {
+        private const double DefaultTolerance = 1e-8;
+
+        public static double GetTolerance<T>(this Iterator<T> iterator)
+             where T : struct, IEquatable<T>, IFormattable
+        {
+            foreach (var item in iterator.StopCriteria)
+            {
+                if (item is ResidualStopCriterion<T> s)
+                {
+                    return s.Tolerance;
+                }
+            }
+
+            return DefaultTolerance;
+        }
+    }
+}
