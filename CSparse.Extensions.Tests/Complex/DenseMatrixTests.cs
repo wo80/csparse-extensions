fa1ff88a725bf82c0bfcf6e78920ee1392282d42
wo80 <christian.woltering@tu-dortmund.de>
2023-05-11 17:03:25 +0200

    Add dense matrix and vector tests.

---
+++ CSparse.Extensions.Tests/Complex/DenseMatrixTests.cs
@@ -0,0 +1,53 @@
+﻿namespace CSparse.Extensions.Tests.Complex
+{
+    using CSparse.Complex;
+    using CSparse.Tests.Complex.Factorization;
+    using NUnit.Framework;
+    using System.Linq;
+    using Complex = System.Numerics.Complex;
+
+    [DefaultFloatingPointTolerance(1e-12)]
+    public class DenseMatrixTests
+    {
+        [Test]
+        public void TestSolve()
+        {
+            var A = DenseLUTests.GetMatrix();
+
+            var x = new DenseVector(Vector.Create(3, Complex.One));
+            var b = new DenseVector(Vector.Create(3, Complex.Zero));
+            var r = new DenseVector(Vector.Create(3, Complex.Zero));
+
+            A.Multiply(x, b);
+            A.Solve(b, r);
+
+            // Comparing complex arrays doesn't respect the floating point tolerance.
+            //CollectionAssert.AreEqual(x, r);
+
+            CollectionAssert.AreEqual(x.Values.Select(a => a.Real), r.Values.Select(a => a.Real));
+            CollectionAssert.AreEqual(x.Values.Select(a => a.Imaginary), r.Values.Select(a => a.Imaginary));
+        }
+
+        [Test]
+        public void TestDeterminant()
+        {
+            var A = DenseLUTests.GetMatrix();
+
+            Assert.AreEqual(24.5, A.Determinant().Real);
+        }
+
+        [Test]
+        public void TestInvert()
+        {
+            var A = DenseLUTests.GetMatrix();
+
+            var inv = new DenseMatrix(A.RowCount, A.ColumnCount);
+
+            A.Inverse(inv);
+
+            var eye = CreateDense.Eye(A.RowCount);
+
+            Assert.IsTrue(eye.Equals(A.Multiply(inv), 1e-12));
+        }
+    }
+}
---
+++ CSparse.Extensions.Tests/Complex/DenseVectorTests.cs
@@ -0,0 +1,69 @@
+﻿namespace CSparse.Extensions.Tests.Complex
+{
+    using CSparse.Complex;
+    using NUnit.Framework;
+    using System;
+    using Complex = System.Numerics.Complex;
+
+    public class DenseVectorTests
+    {
+        [Test]
+        public void TestAdd()
+        {
+            var a = DenseVector.Create(4, 1.0);
+            var b = DenseVector.Create(4, 2.0);
+
+            var expected = DenseVector.Create(4, 3.0);
+            var actual = a.Add(b);
+
+            Assert.AreEqual(expected.Values, actual.Values);
+        }
+
+        [Test]
+        public void TestScale()
+        {
+            var a = DenseVector.Create(4, 2.0);
+
+            var expected = DenseVector.Create(4, 1.0);
+            var actual = a.Scale(0.5);
+
+            Assert.AreEqual(expected.Values, actual.Values);
+        }
+
+        [Test]
+        public void TestDotProduct()
+        {
+            var a = DenseVector.Create(4, 2.0);
+            var b = DenseVector.Create(4, 0.5);
+
+            Assert.AreEqual(C(4.0), a.DotProduct(b));
+        }
+
+        [Test]
+        public void TestNorm()
+        {
+            var a = new DenseVector(new[] { C(0.0), C(-1.0), C(2.0), C(-3.0) });
+
+            Assert.AreEqual(6.0, a.L1Norm());
+            Assert.AreEqual(Math.Sqrt(14.0), a.L2Norm());
+            Assert.AreEqual(3.0, a.InfinityNorm());
+        }
+
+        [Test]
+        public void TestPointwiseMultiply()
+        {
+            var a = DenseVector.Create(3, 2.0);
+            var b = DenseVector.Create(3, 0.5);
+
+            var expected = DenseVector.Create(3, 1.0);
+            var actual = a.PointwiseMultiply(b);
+
+            Assert.AreEqual(expected.Values, actual.Values);
+        }
+
+        private static Complex C(double a, double b = 0.0)
+        {
+            return new Complex(a, b);
+        }
+    }
+}
