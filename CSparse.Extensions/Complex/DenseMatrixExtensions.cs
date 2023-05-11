f5c660fdee8ccb131fe25aa53129b5881b74b927
wo80 <christian.woltering@tu-dortmund.de>
2023-05-11 16:18:40 +0200

    Add DenseMatrix extensions Solve, Inverse and Determinant using LU factorization.

--- CSparse.Extensions/Complex/DenseMatrixExtensions.cs
+++ CSparse.Extensions/Complex/DenseMatrixExtensions.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace CSparse.Complex
 {
+    using CSparse.Complex.Factorization;
     using CSparse.Storage;
     using System.Numerics;
 
@@ -37,6 +38,92 @@ namespace CSparse.Complex
             matrix.TransposeMultiply(x.Values, target.Values);
 
             return target;
+        }
+
+        /// <summary>
+        /// Solves a system of linear equations Ax = b (uses the <see cref="DenseLU"/> factorization).
+        /// </summary>
+        /// <param name="matrix">This matrix.</param>
+        /// <param name="input">Right hand side b.</param>
+        /// <returns>Solution vector x.</returns>
+        public static DenseVector Solve(this DenseColumnMajorStorage<Complex> matrix, DenseVector<Complex> input)
+        {
+            var result = new DenseVector(matrix.RowCount);
+
+            DenseLU.Create(matrix).Solve(input.Values, result.Values);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Solves a system of linear equations Ax = b (uses the <see cref="DenseLU"/> factorization).
+        /// </summary>
+        /// <param name="matrix">This matrix.</param>
+        /// <param name="input">Right hand side b.</param>
+        /// <param name="result">Solution vector x.</param>
+        public static void Solve(this DenseColumnMajorStorage<Complex> matrix, DenseVector<Complex> input, DenseVector<Complex> result)
+        {
+            DenseLU.Create(matrix).Solve(input.Values, result.Values);
+        }
+
+        /// <summary>
+        /// Solves a system of linear equations AX = B (uses the <see cref="DenseLU"/> factorization).
+        /// </summary>
+        /// <param name="matrix">This matrix.</param>
+        /// <param name="input">Right hand side dense matrix B.</param>
+        /// <returns>Dense matrix containing the solution X.</returns>
+        public static DenseMatrix Solve(this DenseColumnMajorStorage<Complex> matrix, DenseColumnMajorStorage<Complex> input)
+        {
+            var result = new DenseMatrix(matrix.RowCount, matrix.ColumnCount);
+
+            DenseLU.Create(matrix).Solve(input, result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Solves a system of linear equations AX = B (uses the <see cref="DenseLU"/> factorization).
+        /// </summary>
+        /// <param name="matrix">This matrix.</param>
+        /// <param name="input">Right hand side dense matrix B.</param>
+        /// <param name="result">Dense matrix containing the solution X.</param>
+        public static void Solve(this DenseColumnMajorStorage<Complex> matrix, DenseColumnMajorStorage<Complex> input, DenseColumnMajorStorage<Complex> result)
+        {
+            DenseLU.Create(matrix).Solve(input, result);
+        }
+
+        /// <summary>
+        /// Returns the inverse of the matrix (uses the <see cref="DenseLU"/> factorization).
+        /// </summary>
+        /// <param name="matrix">This matrix.</param>
+        /// <returns>The inverse of the matrix.</returns>
+        public static DenseMatrix Inverse(this DenseColumnMajorStorage<Complex> matrix)
+        {
+            var result = new DenseMatrix(matrix.RowCount, matrix.ColumnCount);
+
+            DenseLU.Create(matrix).Inverse(result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the inverse of the matrix (uses the <see cref="DenseLU"/> factorization).
+        /// </summary>
+        /// <param name="matrix">This matrix.</param>
+        /// <param name="target">The target matrix containing the inverse on return.</param>
+        public static void Inverse(this DenseColumnMajorStorage<Complex> matrix, DenseColumnMajorStorage<Complex> target)
+        {
+            DenseLU.Create(matrix).Inverse(target);
+        }
+
+        /// <summary>
+        /// Returns the determinant of the matrix (uses the <see cref="DenseLU"/> factorization).
+        /// </summary>
+        /// <param name="matrix">This matrix.</param>
+        /// <returns>The determinant of the matrix.</returns>
+        public static Complex Determinant(this DenseColumnMajorStorage<Complex> matrix)
+        {
+            return DenseLU.Create(matrix).Determinant();
         }
     }
 }
